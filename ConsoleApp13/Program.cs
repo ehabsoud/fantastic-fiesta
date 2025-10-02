b2de11d20014b02f2b30ffc4b65f32acc31bde60
Ehab Soud <ehabsoud24@gmail.com>
2025-10-02 14:38:16 +0200

    tog bort allt

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -4,34 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        string lösenord = "";
-        int försök = 0;
-        int maxFörsök = 3;
-
-        while (försök < maxFörsök)
-        {
-
-            Console.WriteLine("Lösenord: ");
-            lösenord = Console.ReadLine();
-            försök++;
-
-            if (lösenord == "Liverpool")
-            {
-                Console.WriteLine("Välkommen!!!");
-                break;
-            }
-            else
-            {
-                if (försök < maxFörsök)
-                {
-                    Console.WriteLine($"Försök igen, {maxFörsök - försök} kvar");
-                }
-                else
-                {
-                    Console.WriteLine("Fel lösenord, inga försök kvar, försök igen om 5 minuter");
-                }
-            }
-        }
 
     }
 }
