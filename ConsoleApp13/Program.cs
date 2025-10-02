4a791080465c94236d5c805b1e7ec369bda6aa02
Ehab Soud <ehabsoud24@gmail.com>
2025-10-02 14:06:33 +0200

    Min kod

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+        string lösenord = "";
+        
+        while (lösenord != "Liverpool")
+        {
+            Console.WriteLine("Lösenord: ");
+            lösenord = Console.ReadLine();
+
+            if (lösenord == "Liverpool")
+            {
+                Console.WriteLine("Välkommen");
+            }
+            else
+            {
+                Console.WriteLine("Försök igen");
+            }
+        }
+
     }
 }
 
--- ConsoleApp13/obj/Debug/net8.0/ConsoleApp13.AssemblyInfo.cs
+++ ConsoleApp13/obj/Debug/net8.0/ConsoleApp13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c31b64457aa03feb66e86d95daebe6d828420c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
