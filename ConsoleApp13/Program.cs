c69802f93c1963d3330868156848e3f66316cba0
Ehab Soud <ehabsoud24@gmail.com>
2025-10-02 14:22:35 +0200

    Lagt till försök

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -5,19 +5,31 @@ class Program
     static void Main(string[] args)
     {
         string lösenord = "";
+        int försök = 0;
+        int maxFörsök = 3;
         
-        while (lösenord != "Liverpool")
+        while (försök < maxFörsök)
         {
+            
             Console.WriteLine("Lösenord: ");
             lösenord = Console.ReadLine();
+            försök++;
 
             if (lösenord == "Liverpool")
             {
-                Console.WriteLine("Välkommen");
+                Console.WriteLine("Välkommen!");
+                break;
             }
             else
             {
-                Console.WriteLine("Försök igen");
+                if (försök < maxFörsök)
+                {
+                    Console.WriteLine($"Försök igen, {maxFörsök - försök} kvar");
+                }
+                else
+                {
+                    Console.WriteLine("Fel lösenord, inga försök kvar, försök igen om 5 minuter");
+                }
             }
         }
 
